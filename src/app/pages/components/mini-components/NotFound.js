f63676161bf7d2ed935fbce09e3bfc6098c6b27e
aakarkun <aakar.kun@hotmail.com>
2017-09-05 02:57:04 +0545

    Not Found Component Added

---
+++ src/app/pages/components/mini-components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class NotFound extends React.Component {
+    render() {
+        return(
+            <div className="text-center">
+                <img src="https://image.ibb.co/coi2ta/Ayna_04.png" alt="Ayna_04" border="0" height="200" width="145" />
+                <h2>Page Not Found!</h2>
+            </div>
+        );
+    }
+}
