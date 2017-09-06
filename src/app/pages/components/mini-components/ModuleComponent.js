675060ad5a0bc1b5da5186079b00bd172c319891
meemimos <sayabcz@hotmail.com>
2017-09-06 13:45:33 +0545

    module grid bug fixed

--- src/app/pages/components/mini-components/ModuleComponent.js
+++ src/app/pages/components/mini-components/ModuleComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { postUserModule, deleteUserModule } from '../../../utils/users-api';
+import { postUserModule, deleteUserModule, getUserModules } from '../../../utils/users-api';
 import { Spinner } from './Spinner';
 import { MiniSpinner } from './MiniSpinner';
 
@@ -19,15 +19,25 @@ export class ModuleComponent extends React.Component {
             btnStatus: "loading"
         })
         if(this.props.isInstalled === "INSTALL") {
-            const { module, isInstalled } = this.props;            
-            postUserModule(module.name, module.category, module.surface_area, module.position, module.header).then((response, error) => {
-                if(response) {
-                    window.location.reload();
-                    console.log("Module Installed Successfully!");
+            const { module, isInstalled } = this.props;
+            getUserModules().then((modules) => {
+                var modulesName = [];
+                modules.map((module) => {
+                    modulesName.push(module.name);
+                });
+                if(modulesName.indexOf(module.name) === -1) {
+                    postUserModule(module.name, module.category, module.surface_area, module.position, module.header).then((response, error) => {
+                        if(response) {
+                            window.location.reload();
+                            console.log("Module Installed Successfully!");
+                        } else {
+                            console.log("Module Not Installed, Error: " + error);
+                        }
+                    });       
                 } else {
-                    console.log("Module Not Installed, Error: " + error);
+                    console.log("same module found!");
                 }
-            });
+            })
         } else if(this.props.isInstalled === "UNINSTALL"){
             const { module } = this.props;
             deleteUserModule(module._id).then((response, error) => {
@@ -55,25 +65,27 @@ export class ModuleComponent extends React.Component {
                     <div className="badge-circle-bg pull-left"><span style={{paddingLeft: "12px"}}>{module.name.substring(0,2)}</span></div>
                     <div className="media-body" style={{paddingLeft: "12px", }}>
                         <Link to={`/modules/${id}`}><strong style={{fontSize: "16px"}}>{module.name}</strong></Link>
-                        <p>
-                            <strong>Category:</strong> {module.category}<br />
-                            {(isInstalled) === '' ? <Spinner /> :
-                                <div>
-                                    {
-                                        (btnStatus === "loading") ? <MiniSpinner /> :
-                                        <div> 
-                                            {
-                                                (btn_color === "success") ? 
-                                                    <span className={'badge green badge-' + btn_color}>INSTALLED</span> :  
-                                                    <button className={'badge red badge-' + btn_color} onClick={this.handleIsInstalled}>
-                                                        {isInstalled}
-                                                    </button>
-                                            }
+                        <br />
+                        <strong>Category:</strong> {module.category}
+                        <br />
+                        {(isInstalled) === '' ? <Spinner /> :
+                            <div style={{marginTop: "2px", height: "25px"}}>
+                                {
+                                    (btnStatus === "loading") ? <MiniSpinner /> :
+                                        <div>
+                                        {
+                                            (btn_color === "success") ? 
+                                            <button className={'badge green badge-' + btn_color}>
+                                                INSTALLED
+                                            </button>:
+                                            <button className={'badge red badge-' + btn_color} onClick={this.handleIsInstalled}>
+                                                {isInstalled}
+                                            </button>                                                
+                                        }   
                                         </div>
-                                    }
-                                </div>
-                            }
-                        </p>
+                                }
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
